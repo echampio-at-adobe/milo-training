41e3f2da79e9fb44462b78fa2b61b5882654650e
Erich Champion <echampio@adobe.com>
2023-06-21 14:20:01 -0700

    day 3

---
+++ blocks/academy/academy.js
@@ -0,0 +1,10 @@
+export default function init() {
+  const blocks = document.querySelectorAll('.section > div[class]');
+  console.log("init academy);
+  blocks.forEach((block) => {
+    const blockVals = [...block.classList];
+    const blockName = blockVals.shift();
+    block.classList.add('display-block-name');
+    block.dataset.block = `${blockName} (${[...blockVals]})`;
+  });
+}
